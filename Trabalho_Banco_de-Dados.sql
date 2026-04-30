65aa200d84a44a9246df6f361b40842e06ba000a
0Vidal <oliveira.vidal.vinicius@escola.pr.gov.br>
2026-04-30 11:45:46 -0300

    Remove flavor quantity and percentage query
    Removed a query that calculates total quantity and percentage by flavor for the year 2016.

--- Trabalho_Banco_de-Dados.sql
+++ Trabalho_Banco_de-Dados.sql
@@ -33,20 +33,3 @@ on NF.NUMERO = INF.NUMERO
 where year(NF.DATA_VENDA) = 2016
 group by year(NF.DATA_VENDA)
 order by QUANTIDADE desc;
-
-
-
-
-SELECT 
-    TP.SABOR, 
-    SUM(INF.QUANTIDADE) AS QUANTIDADE_TOTAL,
-    CONCAT(ROUND((SUM(INF.QUANTIDADE) / (SELECT SUM(QUANTIDADE) 
-                                         FROM itens_notas_fiscais INF2
-                                         INNER JOIN notas_fiscais NF2 ON INF2.NUMERO = NF2.NUMERO
-                                         WHERE YEAR(NF2.DATA_VENDA) = 2016)) * 100, 2), '%') AS PERCENTUAL
-FROM tabela_de_produtos TP
-INNER JOIN itens_notas_fiscais INF ON TP.CODIGO_DO_PRODUTO = INF.CODIGO_DO_PRODUTO
-INNER JOIN notas_fiscais NF ON INF.NUMERO = NF.NUMERO
-WHERE YEAR(NF.DATA_VENDA) = 2016
-GROUP BY TP.SABOR
-ORDER BY QUANTIDADE_TOTAL DESC;

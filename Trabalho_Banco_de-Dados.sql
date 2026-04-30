b553bb52081daee61385472a0d74bb4b07755e31
0Vidal <oliveira.vidal.vinicius@escola.pr.gov.br>
2026-04-30 09:49:48 -0300

    Update Trabalho_Banco_de-Dados.sql

--- Trabalho_Banco_de-Dados.sql
+++ Trabalho_Banco_de-Dados.sql
@@ -23,6 +23,20 @@ where year(NF.DATA_VENDA) = 2016
 group by TP.SABOR, year(NF.DATA_VENDA)
 order by QUANTIDADE desc;
 
+select year(NF.DATA_VENDA) as ano, 
+sum(INF.QUANTIDADE) as QUANTIDADE
+from tabela_de_produtos as TP
+inner join itens_notas_fiscais as INF
+on TP.CODIGO_DO_PRODUTO = INF.CODIGO_DO_PRODUTO
+inner join notas_fiscais as NF
+on NF.NUMERO = INF.NUMERO
+where year(NF.DATA_VENDA) = 2016
+group by TP.SABOR, year(NF.DATA_VENDA)
+order by QUANTIDADE desc;
+
+
+
+
 SELECT 
     TP.SABOR, 
     SUM(INF.QUANTIDADE) AS QUANTIDADE_TOTAL,

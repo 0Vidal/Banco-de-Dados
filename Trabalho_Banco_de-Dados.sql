1362532871754e5e0e4ffa3d2ab23279edf98c21
0Vidal <oliveira.vidal.vinicius@escola.pr.gov.br>
2026-04-30 08:56:58 -0300

    Fix date_format function in SQL query

--- Trabalho_Banco_de-Dados.sql
+++ Trabalho_Banco_de-Dados.sql
@@ -1,7 +1,7 @@
 use sucos_vendas;
 
 select TP.SABOR, INF.QUANTIDADE,
-date_format(NF.DATA_VENDA, '%Y') as DATA_VENDA
+date_format(NF.DATA_VENDA, '%2016') as DATA_VENDA
 from tabela_de_produtos as TP
 inner join itens_notas_fiscais as INF
 on TP.CODIGO_DO_PRODUTO = INF.CODIGO_DO_PRODUTO
